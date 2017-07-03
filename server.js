a0bb13fc5d4c98e2a757642e3d325d72f2f5e183
LuisPa <LuisPa>
2017-07-03 10:06:01 -0600

    adding headers

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/quotes', (req,res) =>{
     })
 })
 
-app.get("/getvar", function(req, res){
+app.get("/getvar", function(req, res ){
     db.collection('quotes').find().toArray((err, result) => {
         if (err) return console.log(err)
         res.json(result);
